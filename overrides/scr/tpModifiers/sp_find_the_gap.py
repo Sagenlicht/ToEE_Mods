ee13e232f71401e9f98228fc604f10fdf184c8e5
Sagenlicht <herbstgeist@googlemail.com>
2021-08-09 11:29:44 +0200

    added spell_utils to Find the Gap

--- overrides/scr/tpModifiers/sp_find_the_gap.py
+++ overrides/scr/tpModifiers/sp_find_the_gap.py
@@ -2,9 +2,10 @@ from templeplus.pymod import PythonModifier
 from toee import *
 import tpdp
 from utilities import *
+import spell_utils
 print "Registering sp-Find the Gap"
 
-def findTheGapSpellAddTouchFlag(attachee, args, evt_obj):
+def findTheGapSpellCheckTouchAttack(attachee, args, evt_obj):
     if args.get_arg(2): #only the first attack each round ignores armor
         return 0
 
@@ -17,50 +18,21 @@ def findTheGapSpellAddTouchFlag(attachee, args, evt_obj):
         evt_obj.bonus_list.add_cap(33 , 0, 1, "~Find the Gap~[TAG_SPELLS_FIND_THE_GAP]")
         attachee.float_text_line("Find the Gap!")
 
-    args.set_arg(2, 1) #set the flag
+    #Set Touch Attack Flag
+    args.set_arg(2, 1)
     return 0
 
 def findTheGapSpellResetFlag(attachee, args, evt_obj):
     args.set_arg(2, 0)
     return 0
 
-def findTheGapSpellTooltip(attachee, args, evt_obj):
-    if args.get_arg(1) == 1:
-        evt_obj.append("Find the Gap ({} round)".format(args.get_arg(1)))
-    else:
-        evt_obj.append("Find the Gap ({} rounds)".format(args.get_arg(1)))
-    return 0
-
-def findTheGapSpellEffectTooltip(attachee, args, evt_obj):
-    if args.get_arg(1) == 1:
-        evt_obj.append(tpdp.hash("FIND_THE_GAP"), -2, " ({} round)".format(args.get_arg(1)))
-    else:
-        evt_obj.append(tpdp.hash("FIND_THE_GAP"), -2, " ({} rounds)".format(args.get_arg(1)))
-    return 0
-
-def findTheGapSpellHasSpellActive(attachee, args, evt_obj):
-    spellPacket = tpdp.SpellPacket(args.get_arg(0))
-    if evt_obj.data1 == spellPacket.spell_enum:
-        evt_obj.return_val = 1
-    return 0
-
-def findTheGapSpellKilled(attachee, args, evt_obj):
-    args.remove_spell()
-    args.remove_spell_mod()
-    return 0
-
-def findTheGapSpellSpellEnd(attachee, args, evt_obj):
-    print "Find the Gap SpellEnd"
-    return 0
-
-findTheGapSpell = PythonModifier("sp-Find the Gap", 3) # spell_id, duration, notFirstAttack
-findTheGapSpell.AddHook(ET_OnGetAcModifierFromAttacker , EK_NONE, findTheGapSpellAddTouchFlag,())
+findTheGapSpell = PythonModifier("sp-Find the Gap", 4) # spell_id, duration, notFirstAttack, empty
+findTheGapSpell.AddHook(ET_OnGetAcModifierFromAttacker , EK_NONE, findTheGapSpellCheckTouchAttack,())
 findTheGapSpell.AddHook(ET_OnBeginRound, EK_NONE, findTheGapSpellResetFlag, ())
-findTheGapSpell.AddHook(ET_OnGetTooltip, EK_NONE, findTheGapSpellTooltip, ())
-findTheGapSpell.AddHook(ET_OnGetEffectTooltip, EK_NONE, findTheGapSpellEffectTooltip, ())
-findTheGapSpell.AddHook(ET_OnD20Signal, EK_S_Spell_End, findTheGapSpellSpellEnd, ())
-findTheGapSpell.AddHook(ET_OnD20Query, EK_Q_Critter_Has_Spell_Active, findTheGapSpellHasSpellActive, ())
-findTheGapSpell.AddHook(ET_OnD20Signal, EK_S_Killed, findTheGapSpellKilled, ())
+findTheGapSpell.AddHook(ET_OnGetTooltip, EK_NONE, spell_utils.spellTooltip, ())
+findTheGapSpell.AddHook(ET_OnGetEffectTooltip, EK_NONE, spell_utils.spellEffectTooltip, ())
+findTheGapSpell.AddHook(ET_OnD20Query, EK_Q_Critter_Has_Spell_Active, spell_utils.queryActiveSpell, ())
+findTheGapSpell.AddHook(ET_OnD20Signal, EK_S_Killed, spell_utils.spellKilled, ())
 findTheGapSpell.AddSpellDispelCheckStandard()
 findTheGapSpell.AddSpellTeleportPrepareStandard()
 findTheGapSpell.AddSpellTeleportReconnectStandard()
